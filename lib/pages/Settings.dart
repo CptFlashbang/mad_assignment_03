bcc7d78a85c94cc303c24598209f6f390fc0ef79
CptFlashbang <Callum.yates.180@gmail.com>
2024-05-01 14:56:55 +0100

    Refactor settings page in Settings.dart

--- lib/pages/Settings.dart
+++ lib/pages/Settings.dart
@@ -1,64 +1,29 @@
-//Light mode toggle
-
 import 'package:flutter/material.dart';
 
-class SnakePage extends StatefulWidget {
-  const SnakePage({Key? key}) : super(key: key);
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
   static Route<dynamic> route() => MaterialPageRoute(
-        builder: (context) => const SnakePage(),
+        builder: (context) => const SettingsPage(),
       );
   @override
-  _SnakePageState createState() => _SnakePageState();
+  _SettingsPageState createState() => _SettingsPageState();
 }
 
-class _SnakePageState extends State<SnakePage> {
+class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
+    MediaQueryData media;
+    media = MediaQuery.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("About"),
+        title: const Text("Home"),
       ),
-       body: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 3,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[100],
-            child: Image.asset('images/cat1.jpg'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[200],
-            child: Image.asset('images/cat2.jpg'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[300],
-            child: Image.asset('images/cat3.jpg'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[400],
-            child: Image.asset('images/cat4.jpg'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[500],
-            child: Image.asset('images/cat5.jpg'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[600],
-            child: Image.asset('images/cat6.jpg'),
-          ),
-        ],
-      )
+      body: Container(
+        
 
-
-    );
+        ),
+      );
   }
 }
+
