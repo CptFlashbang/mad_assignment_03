278a23a904052665544a6d7b9633043319bc70e5
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-05-02 15:37:14 +0100

    add CRUD functionality to database service

--- lib/database_service.dart
+++ lib/database_service.dart
@@ -1,26 +1,53 @@
 import 'dart:developer';
-
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
-
+import 'attractionModel.dart';
 class DatabaseService {
+  static final DatabaseService _databaseService = DatabaseService._internal();
+  factory DatabaseService() => _databaseService;
+  DatabaseService._internal();
   static Database? _database;
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await initDatabase();
     return _database!;
   }
-
   Future<Database> initDatabase() async {
     final getDirectory = await getApplicationDocumentsDirectory();
-    String path = getDirectory.path + '/savedAttractions.db';
+    String path = getDirectory.path + '/attractions.db';
     log(path);
     return await openDatabase(path, onCreate: _onCreate, version: 1);
   }
-
   void _onCreate(Database db, int version) async {
     await db.execute(
-        'CREATE TABLE Attractions(id TEXT PRIMARY KEY, name TEXT, saved BOOL)');
+    'CREATE TABLE Attractions(id TEXT PRIMARY KEY, name TEXT, saved BOOL)');
     log('TABLE CREATED');
+  }
+  Future<List<AttractionModel>> getAttractions() async {
+    final db = await _databaseService.database;
+    var data = await db.rawQuery('SELECT * FROM Attractions');
+    List<AttractionModel> attractions =
+    List.generate(data.length, (index) => AttractionModel.fromJson(data[index]));
+    print(attractions.length);
+    return attractions;
+  }
+  Future<void> insertMovie(AttractionModel movie) async {
+    final db = await _databaseService.database;
+    var data = await db.rawInsert(
+        'INSERT INTO Attractions(id, name, saved, year ) VALUES(?,?,?,?)',
+        [movie.id, movie.name, movie.saved]);
+    log('inserted $data');
+  }
+  // Future<void> editMovie(AttractionModel movie) async {
+  //   final db = await _databaseService.database;
+  //   var data = await db.rawUpdate(
+  //       'UPDATE Attractions SET name=?,saved=?,year=? WHERE ID=?',
+  //       [movie.name, movie.saved, movie.id]);
+  //   log('updated $data');
+  // }
+  Future<void> deleteMovie(String id) async {
+    final db = await _databaseService.database;
+    var data = await db.rawDelete('DELETE from Attractions WHERE id=?', [id]);
+    log('deleted $data');
   }
 }
